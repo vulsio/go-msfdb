f3ec8327dc0f4f5dd5e929165f0753c528224570
takuzoo <sata3868.k.serpentis@gmail.com>
2020-03-03 16:57:35 +0900

    :sparkles: add DB models

---
+++ db/db.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/inconshreveable/log15"
+	"github.com/takuzoo3868/go-msfdb/models"
+)
+
+type DB interface {
+	Name() string
+	OpenDB(dbType, dbPath string, debugSQL bool) (bool, error)
+	MigrateDB() error
+}
+
+func NewDB(dbType string, dbPath string, debugSQL bool) (driver DB, locked bool, err error) {
+	if driver, err = newDB(dbType); err != nil {
+		log15.Error("Failed to new db.", "err", err)
+		return driver, false, err
+	}
+
+	log15.Info("Opening Database.", "db", driver.Name())
+	if locked, err := driver.OpenDB(dbType, dbPath, debugSQL); err != nil {
+		if locked {
+			return nil, true, err
+		}
+		return nil, false, err
+	}
+
+	log15.Info("Migrating DB.", "db", driver.Name())
+	if err := driver.MigrateDB(); err != nil {
+		log15.Error("Failed to migrate db.", "err", err)
+		return driver, false, err
+	}
+	return driver, false, nil
+}
+
+func newDB(dbType string) (DB, error) {
+	switch dbType {
+	case dialectSqlite3, dialectMysql, dialectPostgreSQL:
+		return &RDBDriver{name: dbType}, nil
+	case dialectRedis:
+		return &RedisDriver{name: dbType}, nil
+	}
+	return nil, fmt.Errorf("Invalid database dialect, %s", dbType)
+}
