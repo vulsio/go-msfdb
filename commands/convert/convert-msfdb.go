7e32833d9f407ee54eec5fde50a5f09abac2df49
MaineK00n <mainek00n.1229@gmail.com>
2022-01-07 13:00:13 +0900

    refactor: change early return

--- commands/convert/convert-msfdb.go
+++ commands/convert/convert-msfdb.go
@@ -42,12 +42,11 @@ func convertMetasploitDB(_ *cobra.Command, _ []string) (err error) {
 
 	vulnDir := viper.GetString("vuln-dir")
 	if f, err := os.Stat(vulnDir); err != nil {
-		if os.IsNotExist(err) {
-			if err := os.MkdirAll(vulnDir, 0700); err != nil {
-				return xerrors.Errorf("Failed to create vuln directory. err: %w", err)
-			}
-		} else {
-			return xerrors.Errorf("Failed to check vuln directory. err: %w", err)
+		if !os.IsNotExist(err) {
+			return xerrors.Errorf("Failed to check vuln directory. err: %w", err)			
+		}
+		if err := os.MkdirAll(vulnDir, 0700); err != nil {
+			return xerrors.Errorf("Failed to create vuln directory. err: %w", err)
 		}
 	} else if !f.IsDir() {
 		return xerrors.Errorf("Failed to check vuln directory. err: %s is not directory", vulnDir)
